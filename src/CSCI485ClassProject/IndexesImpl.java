42f63565966d5810d5fe181f955ad359e2b41a90
Andre Hsueh <andrehsu@usc.edu>
2023-04-12 00:59:56 -0700

    Update IndexesImpl.java

--- src/CSCI485ClassProject/IndexesImpl.java
+++ src/CSCI485ClassProject/IndexesImpl.java
@@ -60,7 +60,9 @@ public class IndexesImpl implements Indexes{
     List<String> table = new ArrayList<>();
     table.add(tableName);
     table.add(attrName);
+    FDBHelper.openSubspace(tx, table);
     FDBHelper.dropSubspace(tx, table);
+    FDBHelper.commitTransaction(tx);
     return StatusCode.SUCCESS;
   }
 }
